8fe316525e9560614b5fb273ed3d3a3c89da8da3
rystars <1127830301@qq.com>
2023-04-26 23:33:41 +0800

    learn#230426 Life

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <ShoppingDemo></ShoppingDemo>
+    <Life></Life>
   </div>
 </template>
 
 <script setup lang='ts'>
   // import RefDemo from './components/1-Ref.vue';
   // import ReactiveDemo from './components/2-reactive.vue';
-  import ShoppingDemo from './components/3-shopping.vue';
+  // import ShoppingDemo from './components/3-shopping.vue';
+  import Life from './components/4-life.vue';
 </script>
 
 <style scoped>
---
+++ src/components/4-life.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div  ref="div1">通过ref获取dom信息 {{ msg }} </div>
+    <el-button type="success" @click="update">{{ msg }}</el-button>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import {ref,onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted} from 'vue';
+console.log('setup1');
+const div1 = ref<HTMLDivElement>();
+const msg = ref<string>('我还没被更新');
+// 创建之前
+onBeforeMount(() => {
+    console.log('创建之前',div1.value);
+})
+// 创建之后
+onMounted(()=>{
+    console.log('创建之后',div1.value);
+    
+})
+
+const update = () => {
+    msg.value = '我被更新了'
+}
+
+console.log('setup2');
+// 更新之前
+onBeforeUpdate(()=>{
+    console.log('更新之前',div1.value?.innerText);
+    
+})
+// 更新之后
+onUpdated(()=>{
+    console.log('更新之后',div1.value?.innerText);
+    
+})
+console.log('setup3');
+// 销毁之前
+onBeforeUnmount(()=>{
+    console.log('销毁之前');
+    
+})
+// 销毁之后
+onUnmounted(()=>{
+    console.log('销毁之后');
+    
+})
+console.log('setup4');
+
+
+</script>
+
+<style scoped>
+
+</style>
